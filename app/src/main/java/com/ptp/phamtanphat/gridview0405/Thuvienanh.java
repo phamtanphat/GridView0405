b7810bb89c9b8b70e392c3bb98e40915a280c367
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-01 18:10:12 +0700

    "Update"

---
+++ app/src/main/java/com/ptp/phamtanphat/gridview0405/Thuvienanh.java
@@ -0,0 +1,21 @@
+package com.ptp.phamtanphat.gridview0405;
+
+/**
+ * Created by KhoaPhamPC on 1/6/2018.
+ */
+
+public class Thuvienanh {
+    private int Hinhanh;
+
+    public Thuvienanh(int hinhanh) {
+        Hinhanh = hinhanh;
+    }
+
+    public int getHinhanh() {
+        return Hinhanh;
+    }
+
+    public void setHinhanh(int hinhanh) {
+        Hinhanh = hinhanh;
+    }
+}
---
+++ app/src/main/java/com/ptp/phamtanphat/gridview0405/ThuvienanhAdapter.java
@@ -0,0 +1,8 @@
+package com.ptp.phamtanphat.gridview0405;
+
+/**
+ * Created by KhoaPhamPC on 1/6/2018.
+ */
+
+public class ThuvienanhAdapter {
+}
